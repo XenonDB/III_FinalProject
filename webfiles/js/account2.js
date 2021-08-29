548c19ddc09ebea6cf71052176242a7622875009
XenonDB <58476103+XenonDB@users.noreply.github.com>
2021-08-29 20:05:28 +0800

    更新網頁相關函式庫

---
+++ webfiles/js/account2.js
@@ -0,0 +1,103 @@
+$(function(){
+	
+	healthyLifeStyleDBUtil.getCurrentLoginAccount(function(data){
+		$("#loginAccount").html(data.user);
+	});
+	
+});
+
+function buildTable(data){
+
+	var initStr = 
+		`<thead>
+			<tr>
+				<td>使用者帳戶</td>
+				<td>暱稱</td>
+			</tr>
+		</thead>`;
+	
+	
+	$("#memberTable").html(initStr);
+	$("#adminTable").show();
+	
+	data.forEach(function(e){
+		
+		var col1 = $("<td>").html(e.user);
+		var col2 = $("<td>").html(e.nickName);
+		
+		var row = $("<tr>").append(col1).append(col2);
+		
+		$("#memberTable").append(row);
+	
+	});
+	
+	$("#memberTable").DataTable();
+
+}
+
+function getRegisteredMembers(){
+
+	healthyLifeStyleDBUtil.getRegisteredMembers(function(data){buildTable(data);});
+
+}
+
+function logout(){
+
+	healthyLifeStyleDBUtil.logOut();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function deleteAccount(){
+	
+	var account = $("#accountToDelete")[0].value;
+	
+	if(!account) return;
+	
+	var formData = "accountToDelete="+account;
+	var requestUrl = window.location.origin + "/HealthyLifestyle/Account/Admin/MemberManager";
+	$.post({
+		url: requestUrl,
+		data: formData
+	}).done(function(){
+		alert("成功刪除帳號!!");
+	}).fail(function(data){
+		var errmsg = "";
+		switch(data.status){
+			case 401:
+				errmsg = "無權限的操作。";
+				break;
+			case 404:
+				errmsg = "伺服器沒有回應，無法刪除帳號。";
+				break;
+			case 400:
+				errmsg = "無法刪除不存在的帳號。";
+				break
+			default:
+				errmsg = "伺服器發生未預期錯誤。";
+		}
+		alert(errmsg);
+	});
+
+}
